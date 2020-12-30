091b289a7c3ab15b48b8b9d18b848390a47ed5fd
CH.Hannan <ch.hannan15@gmail.com>
2020-12-30 23:19:23 +0500

    Fixed bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AppRoutingModule} from 'src/app/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AlbumPageComponent } from './album-page/album-page.component';
+import { PhotosPageComponent } from './photos-page/photos-page.component';
+import { PhotoCreatorComponent } from './photo-creator/photo-creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumPageComponent,
+    PhotosPageComponent,
+    PhotoCreatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
